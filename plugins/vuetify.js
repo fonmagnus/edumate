2f8d28c7beefb9e8084536ae65088ed1362b62ae
fonmagnus <arnold777armageddon@gmail.com>
2018-10-18 23:23:46 +0700

    creating home page

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -4,12 +4,13 @@ import colors from 'vuetify/es5/util/colors'
 
 Vue.use(Vuetify, {
   theme: {
-    primary: '#121212', // a color that is not in the material colors palette
+    primary: '#212121', 
     accent: colors.grey.darken3,
-    secondary: colors.amber.darken3,
+    secondary: '#212121',
     info: colors.teal.lighten1,
-    warning: colors.amber.base,
-    error: colors.deepOrange.accent4,
-    success: colors.green.accent3
-  }
+    warning: '#FFD54F',
+    error: '#E65100',
+    success: '#AEEA00',
+  },
+  iconfont: 'fa',
 })
